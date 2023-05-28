3a0e63710b75f1075e0b2336f952555803bcc2a1
Joas Yannick Kinouani <joasyannick@gmail.com>
2023-05-28 18:49:48 +0200

    Add logo and favicon support

--- functions.php
+++ functions.php
@@ -1,23 +1,25 @@
 <?php
+    add_theme_support( 'custom-logo' );
+
     function vspa_add_vue_css() {
         $css = '/assets/app/index.css';
         wp_enqueue_style(
-            'vspa-vue',
-            get_template_directory_uri() . $css,
-            array(),
-            date( 'Y.m.d.H.i.s', filemtime( get_template_directory() . $css ) )
-        );
+                'vspa-vue',
+                get_template_directory_uri() . $css,
+                array(),
+                date( 'Y.m.d.H.i.s', filemtime( get_template_directory() . $css ) )
+            );
     }
     add_action( 'wp_enqueue_scripts', 'vspa_add_vue_css' );
 
     function vspa_add_vue_js() {
         $js = '/assets/app/index.js';
         wp_enqueue_script(
-            'vspa-vue',
-            get_template_directory_uri() . $js,
-            array(),
-            date( 'Y.m.d.H.i.s', filemtime( get_template_directory() . $js ) )
-        );
+                'vspa-vue',
+                get_template_directory_uri() . $js,
+                array(),
+                date( 'Y.m.d.H.i.s', filemtime( get_template_directory() . $js ) )
+            );
     }
     function vspa_add_vue_js_html_attributes( $tag, $handle, $src ) {
         $prefix = 'vspa-vue';
@@ -31,12 +33,17 @@
 
     function vspa_extend_rest_api() {
         register_rest_route( 'vue-spa-starter/v1', '/logo', array(
-            'methods' => WP_REST_Server::READABLE,
-            'callback' => function( $request ) {
-                return rest_ensure_response( get_template_directory_uri() . '/assets/logo.svg' );
-            },
-            'permission_callback' => '__return_true'
-          ) );
+                'methods' => WP_REST_Server::READABLE,
+                'callback' => function( $request ) {
+                    $custom_logo_id = get_theme_mod( 'custom_logo' );
+                    if ( has_custom_logo() ) {
+                        return rest_ensure_response( esc_url( wp_get_attachment_image_url( $custom_logo_id, 'full' ) ) );
+                    } else {
+                        return rest_ensure_response( esc_url( get_template_directory_uri() . '/assets/logo.svg' ) );
+                    }
+                },
+                'permission_callback' => '__return_true'
+            ) );
     }
     add_action( 'rest_api_init', 'vspa_extend_rest_api' );
 ?>
--- header.php
+++ header.php
@@ -3,6 +3,14 @@
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="icon" href="<?php
+                $favicon_id = (int) get_option( 'site_icon' );
+                echo esc_url(
+                        $favicon_id?
+                            wp_get_attachment_image_url( $site_icon_id, 'full' ):
+                            get_template_directory_uri() . '/assets/favicon.ico'
+                    );
+            ?>"/>
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
